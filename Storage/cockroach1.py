f9615a9095305650d5eb9aa8592643778a77c65b
below_v <100206243+bor1sor@users.noreply.github.com>
2023-08-23 12:48:26 +0300

    1
    2

--- Storage/cockroach1.py
+++ Storage/cockroach1.py
@@ -1,23 +1,23 @@
-# Высота вентиляционной шахты
-mine = 4
-# Сколько таракан проползает за час
-speed = 2
-# Расстояние, на которое сползает уставший таракан
+# Ventilation shaft height
+mine = 3
+# How many cockroaches crawl in an hour
+speed = 1.8
+# How many meters does a cockroach crawl in an hour?
 slowdown = 2 / 3
-# Сколько метров прополз таракан
-done = 0
-# Сколько часов он на это потратил
-hours = 0
-# Пока пройденное расстояние меньше длины шахты — ползём дальше
+# How many meters did the cockroach crawl
+done = 1
+# How many hours did he spend on it?
+hours = 0.3
+# As long as the distance traveled is less than the length of the mine, we crawl further
 while done <= mine:
-    # Прошёл час
+    # An hour passed
     hours += 1
-    # За этот час таракан прополз сколько-то метров
+    # During this hour, the cockroach crawled some meters
     done += speed
-    # Если он дополз до края — принудительно останавливаем цикл
+    # If it crawled to the edge, we forcibly stop the cycle
     if done >= mine:
         break
-    # Усталый таракан в конце часа скатывается вниз
+    # A tired cockroach rolls down at the end of the hour
     done -= slowdown
-# Выводим количество полученных часов
-print("Таракану понадобится", hours, "часов.")
+# Output the number of hours received
+print("The cockroach will need", hours, "hours.")
