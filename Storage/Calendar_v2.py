ae5d6f4cc65121f86bf43369a6cbbf5e7be80e21
below_v <100206243+bor1sor@users.noreply.github.com>
2023-10-11 13:43:26 +0300

    1

--- Storage/Calendar_v2.py
+++ Storage/Calendar_v2.py
@@ -1,9 +1,5 @@
 import calendar
 
-May = calendar.month(2023, 5)
-June = calendar.month(2022, 6)
-August = calendar.month(2024, 8)
+May = calendar.month(2023, 12)
 
 print(May)
-print(June)
-print(August)
