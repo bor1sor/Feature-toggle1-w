7a0e8e150a0a02892a3b743d6cab2f8ee5822bed
below_v <100206243+bor1sor@users.noreply.github.com>
2024-08-03 00:51:49 +0300

    1
    1

--- Storage/list.py
+++ Storage/list.py
@@ -2,10 +2,10 @@ import requests
 
 
 def test_website_availability():
-    url = "https://shop.budu.ru/002"
+    url = "https://shop.budu.ru/"
     response = requests.get(url)
     assert (
-        response.status_code == 200
+        response.status_code == 404
     ), f"Failed to access {url}, Status code: {response.status_code}"
 
 
