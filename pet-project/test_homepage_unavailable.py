289389c7b65e298772e87b2a9e46ccc14e2ed1fc
below_v <100206243+bor1sor@users.noreply.github.com>
2024-08-14 00:39:13 +0300

    2
    2

---
+++ pet-project/test_homepage_unavailable.py
@@ -0,0 +1,14 @@
+import requests
+
+
+def test_homepage_unavailable():
+    url = "https://shop.budu.ru/catalog/0000"
+    response = requests.get(url)
+    assert (
+        response.status_code == 200
+    ), f"Страница недоступна, статус: {response.status_code}"
+    print("Page not found.")
+
+
+if __name__ == "__main__":
+    test_homepage_unavailable()
