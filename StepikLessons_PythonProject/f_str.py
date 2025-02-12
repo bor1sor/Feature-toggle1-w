619148752de20d60239d60ed19177b16e4ba836e
below_v <100206243+bor1sor@users.noreply.github.com>
2025-02-12 11:08:33 +0300

    upd1
    1

--- StepikLessons_PythonProject/f_str.py
+++ StepikLessons_PythonProject/f_str.py
@@ -1,4 +1,9 @@
 name = "Paul"
 age = 31
 
-print("My name is " + name + ", im " + str(age) + " years old")
+# print("My name is " + name + ", im " + str(age) + " years old")
+# print(f"My name is {name}, Im {age} years old")
+
+num1 = 10
+num2 = 50
+print(f"Value Is {num1+num2}")
