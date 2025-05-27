97bd04894f8af67e26269196894b593ef7a5fd7d
below_v <100206243+bor1sor@users.noreply.github.com>
2025-05-27 19:14:22 +0300

    1

---
+++ newProject/src/SimpleSegmentationExample.java
@@ -0,0 +1,12 @@
+public class SimpleSegmentationExample {
+
+    public static void main(String[] args) {
+
+        int a = 30;
+        int b = 10;
+        int result = a / b;
+
+        System.out.println("результат: " + result);
+    }
+
+}
