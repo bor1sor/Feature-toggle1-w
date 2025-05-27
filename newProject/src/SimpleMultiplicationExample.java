86c6c0bef047faefef12a7417b935bf764c381c9
below_v <100206243+bor1sor@users.noreply.github.com>
2025-05-27 19:05:52 +0300

    1

---
+++ newProject/src/SimpleMultiplicationExample.java
@@ -0,0 +1,12 @@
+public class SimpleMultiplicationExample {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 8;
+        int result = a * b;
+
+        System.out.println("результат: " + result);
+    }
+
+}
