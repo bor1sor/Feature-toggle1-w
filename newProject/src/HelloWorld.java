518858472d6afb03aa5ea4a7111503bba490f1ab
below_v <100206243+bor1sor@users.noreply.github.com>
2025-06-10 17:40:35 +0300

    HW

--- newProject/src/HelloWorld.java
+++ newProject/src/HelloWorld.java
@@ -2,6 +2,15 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
+        String greeting = "Hello, World!";
+
+        printGreeting(greeting);
+
+    }
+
+    public static void printGreeting(String message) {
+
+        System.out.println(message);
     }
 
 }
